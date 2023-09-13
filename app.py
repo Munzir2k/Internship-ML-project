74c13162bc02e3d492f88ff5fe18887dd29adc22
Mohammad Munzir <mohammadmunzir2k@gmail.com>
2023-09-13 11:27:31 +0530

    final commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 # import matplotlib
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model/model.pkl', 'rb'))
 @app.route('/', methods=['GET'])
 def Home():
     return render_template('index.html')
